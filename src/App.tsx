3a5b600d854e9212782f5ddfab72e19d01578e3a
Thomas <thomas.a.schick@gmail.com>
2021-08-19 21:57:24 +0200

    Initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,108 @@
-import React from 'react';
+import { FC, useCallback, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { produce } from 'immer'
 
-function App() {
+const ROWS = 25;
+const COLUMNS = 25;
+
+// Moore neighborhood
+const operations = [
+  [-1, -1],  // NW
+  [-1, 0],   // N
+  [-1, 1],   // NE
+  [0, 1],    // E
+  [1, 1],    // SE
+  [1, 0],    // S
+  [1, -1],   // SW
+  [0, -1],   // W
+];
+
+const App: FC = () => {
+  // const [grid, setGrid] = useState<any[][]>(Array.from({ length: ROWS }).map(() => Array.from({ length: COLUMNS }).fill(0)))
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < ROWS; i++) {
+      rows.push(Array.from(Array(COLUMNS), () => 0));
+    }
+    return rows;
+  });
+
+  const [running, setRunning] = useState(false);
+
+  // make the value of running available in useCallback
+  const runningRef = useRef(running);
+  runningRef.current = running;
+
+  const run = useCallback(() => {
+    console.log(runningRef.current);
+    if (!runningRef.current) return;
+
+    // simulate
+    setGrid((grid) => produce(grid, gridCopy => {
+      for (let i = 0; i < ROWS; i++) {
+        for (let j = 0; j < COLUMNS; j++) {
+          let neighbors = 0;
+          operations.forEach(([x, y]) => {
+            const newI = i + x;
+            const newJ = j + y;
+
+            // check bounds
+            if (newI >= 0 && newI < ROWS && newJ >= 0 && newJ < COLUMNS) {
+              neighbors += grid[newI][newJ];
+            }
+
+            // game of life rules
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][j] = 0;
+            } else if (grid[i][j] === 0 && neighbors === 3) {
+              gridCopy[i][j] = 1;
+            }
+          })
+        }
+      }
+    }));
+
+    setTimeout(run, 3100);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button
+        onClick={() => {
+          setRunning(!running);
+          if (!running) {
+            // state update might not be done in time
+            runningRef.current = true;
+            run();
+          }
+        }}
+      >
+        {running ? 'stop' : 'start'}
+      </button>
+      <div style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat(${COLUMNS}, 20px)`
+      }}>{grid.map((rows, rowIndex) =>
+        rows.map((col, colIndex) => (
+          <div
+            key={`${rowIndex}-${colIndex}`}
+            onClick={() => {
+              // we dont want to mutate, so we produce a copy
+              const newGrid = produce(grid, gridCopy => {
+                gridCopy[rowIndex][colIndex] = gridCopy[rowIndex][colIndex] ? 0 : 1;
+              });
+              setGrid(newGrid);
+            }}
+            style={{
+              width: 20,
+              height: 20,
+              backgroundColor: grid[rowIndex][colIndex] ? "yellow" : undefined,
+              border: "solid 1px black"
+            }} />
+        )))}
+      </div>
+    </>
   );
 }
 
